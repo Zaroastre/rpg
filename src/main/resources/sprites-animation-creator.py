33478cc416a4efa08395bc9dcbaf05a30a720512
Nicolas METIVIER <nicolas.a.metivier@gmail.com>
2023-11-10 19:54:42 +0100

    Renaming

--- src/main/resources/sprites-animation-creator.py
+++ src/main/resources/sprites-animation-creator.py
@@ -205,6 +205,17 @@ class Geometry:
 
         return Position((rotated_x + rotation_center.x), (rotated_y + rotation_center.y))
 
+class Bone:
+    def __init__(self, start: Position, end: Position, thickness: float) -> None:
+        self.__start: Position = start
+        self.__end: Position = end
+        self.__thickness: float = thickness
+        self.__parent: Optional[Bone] = Optional.empty()
+        self.__children: list[Bone] = []
+
+class Skeleton:
+    def __init__(self) -> None:
+        self.__bones: list[Bone] = []
 
 class Point(pygame.sprite.Sprite, Draw,InputEventHandler):
     """_summary_
@@ -220,22 +231,33 @@ class Point(pygame.sprite.Sprite, Draw,InputEventHandler):
     def __init__(self, position: Position) -> None:
         pygame.sprite.Sprite.__init__(self)
         Point.TOTAL_CREATED += 1
-        self.name: str = "PNT-#"+str(Point.TOTAL_CREATED)
-        self.radius: float = 10.0
-        self.texture = pygame.Surface([self.radius*2, self.radius*2])
+        self.__name: str = "PNT-#"+str(Point.TOTAL_CREATED)
+        self.__radius: float = 10.0
+        self.__texture = pygame.Surface([self.__radius*2, self.__radius*2])
         self.parent: Point|None = None
         self.position: Position = position
         self.__is_selected: bool = False
-        self.hitbox: pygame.Rect = None
-        self.font_size: int = 20
-        self.font: pygame.font.Font = pygame.font.Font(None, self.font_size)
-        self.font_color: pygame.Color = pygame.Color(255,255,255)
-        self.title: pygame.Surface = self.font.render(self.name, True, self.font_color)
-        self.children: list[Point] = []
+        self.__hitbox: pygame.Rect = None
+        self.__font_size: int = 20
+        self.__font: pygame.font.Font = pygame.font.Font(None, self.__font_size)
+        self.__font_color: pygame.Color = pygame.Color(255,255,255)
+        self.__title: pygame.Surface = self.__font.render(self.name, True, self.__font_color)
+        self.__children: list[Point] = []
+
+    def get_children(self) -> list:
+        return self.__children
 
     def is_selected(self) -> bool:
         return self.__is_selected
 
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def hitbox(self) -> pygame.Rect:
+        return self.__hitbox
+    
     def select(self):
         self.__is_selected = True
     
@@ -243,63 +265,90 @@ class Point(pygame.sprite.Sprite, Draw,InputEventHandler):
         self.__is_selected = False
 
     def handle(self, event: pygame.event.Event):
-        mouse_position = pygame.mouse.get_pos()
+        mouse_position: tuple[int, int] = pygame.mouse.get_pos()
         if (event.type == pygame.MOUSEBUTTONDOWN):
-            if (self.hitbox.collidepoint(mouse_position)):
-                print(f"POINT {self.name} is selected")
+            if (self.__hitbox.collidepoint(mouse_position)):
+                print(f"POINT {self.__name} is selected")
                 self.select()
 
-    def draw(self, master: pygame.Surface):
-        self.texture.fill(Point.TRANSPARENT)
-        color: pygame.Color
+    def draw(self, surface: pygame.Surface):
+        self.__texture.fill(Point.TRANSPARENT)
+        point_color: pygame.Color
         if (self.is_selected()):
-            color = Point.SELECTED_COLOR
+            point_color = Point.SELECTED_COLOR
         else:
-            color = Point.UNSELECTED_COLOR
-        self.hitbox = pygame.draw.circle(self.texture, color, (self.radius, self.radius), self.radius)
-        self.hitbox = master.blit(self.texture, (self.position.x-self.radius,self.position.y-self.radius))
-        master.blit(self.title, (self.position.x+self.radius*2,self.position.y-(self.radius/2)))
+            point_color = Point.UNSELECTED_COLOR
+        self.__hitbox = pygame.draw.circle(self.__texture, point_color, (self.__radius, self.__radius), self.__radius)
+        self.__hitbox = surface.blit(self.__texture, (self.position.x-self.__radius,self.position.y-self.__radius))
+        surface.blit(self.__title, (self.position.x+self.__radius*2,self.position.y-(self.__radius/2)))
 
     def copy(self):
         copy: Point = Point(Position(self.position.x, self.position.y, self.position.z))
         return copy
 
     def __repr__(self) -> str:
-        return f"{self.name}(P={self.parent})"
+        return f"{self.__name}(P={self.parent})"
 
 class Frame(Draw):
-    def __init__(self) -> None:
-        self.points: list[Point] = []
-        self.is_selected: bool = False
+    def __init__(self, skeleton: Skeleton) -> None:
+        self.__skeleton: Skeleton = skeleton
+        self.__points: list[Point] = []
+        self.__is_selected: bool = False
+
+    @property
+    def skeleton(self) -> Skeleton:
+        return self.__skeleton
+    
+    @property
+    def is_selected(self) -> bool:
+        return self.__is_selected
+
+    def get_points(self) -> list[Point]:
+        return self.__points.copy()
+
+    def add_point(self, point: Point):
+        self.__points.append(point)
+        
+    def delete_point(self, point: Point):
+        self.__points.remove(point)
+
+    def delete_all_points(self):
+        self.__points.clear()
+
+    def select(self):
+        self.__is_selected = True
+    def unselect(self):
+        self.__is_selected = False
 
     def copy(self):
         copies_associations: dict = {}
-        dupliacted_frame: Frame = Frame()
-        for point in self.points:
+        dupliacted_frame: Frame = Frame(skeleton=None)
+        for point in self.get_points():
             duplicated_point: Point = point.copy()
             copies_associations[point.name] = duplicated_point.name
-            dupliacted_frame.points.append(duplicated_point)
-        for point in self.points:
+            dupliacted_frame.add_point(duplicated_point)
+        for point in self.get_points():
             if (point.parent is not None):
                 dupliacted_point_name: str = copies_associations.get(point.name)
                 dupliacted_parent_point_name: str = copies_associations.get(point.parent.name)
-                copied_points_with_name: list[Point] = [duplicated_point for duplicated_point in dupliacted_frame.points if duplicated_point.name == dupliacted_point_name]
-                copied_parent_points_with_name: list[Point] = [duplicated_point for duplicated_point in dupliacted_frame.points if duplicated_point.name == dupliacted_parent_point_name]
+                copied_points_with_name: list[Point] = [duplicated_point for duplicated_point in dupliacted_frame.get_points() if duplicated_point.name == dupliacted_point_name]
+                copied_parent_points_with_name: list[Point] = [duplicated_point for duplicated_point in dupliacted_frame.get_points() if duplicated_point.name == dupliacted_parent_point_name]
                 if (len(copied_points_with_name) > 0 and len(copied_parent_points_with_name) > 0):
                     copied_points_with_name[0].parent = copied_parent_points_with_name[0]
                 
         return dupliacted_frame
 
-    def __draw_points(self, master: pygame.Surface):
-        for point in self.points:
-            point.draw(master)
+    def __draw_points(self, surface: pygame.Surface):
+        for point_to_draw in self.get_points():
+            point_to_draw.draw(surface)
 
-    def __draw_line_between_each_points(self, master: pygame.Surface):
-        if (len(self.points) > 1):
-            for point in self.points:
+    def __draw_line_between_each_points(self, surface: pygame.Surface):
+        if (len(self.get_points()) > 1):
+            for point in self.get_points():
                 if (point.parent is not None):
                     parent: Point = point.parent
-                    pygame.draw.line(master, pygame.Color(255,255,255), (parent.position.x, parent.position.y), (point.position.x, point.position.y))
+                    pygame.draw.line(surface, pygame.Color(255,255,255), (parent.position.x, parent.position.y), (point.position.x, point.position.y))
+
     def draw(self, master: pygame.Surface):
         self.__draw_points(master)
         self.__draw_line_between_each_points(master)
@@ -309,7 +358,7 @@ class Timeline(pygame.sprite.Sprite, Draw, InputEventHandler):
     FRAME_MARGIN: int = 5
     def __init__(self, width: float, height: float, position: Position) -> None:
         pygame.sprite.Sprite.__init__(self)
-        self.frames: list[Frame] = []
+        self.__frames: list[Frame] = []
         self.texture = pygame.Surface([width, height])
         self.position: Position = position
         self.background_color: pygame.Color = pygame.Color(50, 50, 50)
@@ -325,61 +374,78 @@ class Timeline(pygame.sprite.Sprite, Draw, InputEventHandler):
         self.font_color: pygame.Color = pygame.Color(255,255,255)
         self.title: pygame.Surface = self.font.render("Timeline", True, self.font_color)
     
+    def get_frames(self) -> list[Frame]:
+        return self.__frames.copy()
+    
     def add_event_listener_on_selected_frame_change(self, callback):
         self.__on_selected_frame_change_callback = callback
     
+    def add_frame(self, frame_to_add: Frame):
+        self.__frames.append(frame_to_add)
+    
     def select_frame(self, frame_to_select: Frame):
         print("Select FRAME")
-        if (frame_to_select in self.frames):
-            for frame in self.frames:
-                frame.is_selected = False
-            frame_to_select.is_selected = True
+        if (frame_to_select in self.get_frames()):
+            for frame in self.get_frames():
+                frame.unselect()
+            frame_to_select.select()
             self.selected_frame = Optional.of_nullable(frame_to_select)
             if (self.__on_selected_frame_change_callback is not None):
                 self.__on_selected_frame_change_callback(frame_to_select)
+    
+    def delete_frame(self, frame_to_delete: Frame):
+        if (frame_to_delete in self.__frames):
+            self.__frames.remove(frame_to_delete)
+    
+    def select_previous_frame(self):
+        index: int = self.__frames.index(self.selected_frame.get())
+        if (index > 0):
+            self.select_frame(self.__frames[index-1])
+            
+    def select_next_frame(self):
+        index: int = self.__frames.index(self.selected_frame.get())
+        if (index < (len(self.__frames)-1)):
+            self.select_frame(self.__frames[index+1])
     
     def __handle_duplicate_frame(self):
         print("Duplicate FRAME")
         if (self.selected_frame.is_present()):
             frame: Frame = self.selected_frame.get()
             new_frame: Frame = frame.copy()
-            self.frames.append(new_frame)
+            self.__frames.append(new_frame)
             self.select_frame(new_frame)
 
     def __handle_delete_frame(self):
         print("Delete FRAME")
         if (self.selected_frame.is_present()):
             frame_to_delete: Frame = self.selected_frame.get()
-            index: int = self.frames.index(frame_to_delete)
+            index: int = self.__frames.index(frame_to_delete)
             if (index >= 0):
-                self.frames.remove(frame_to_delete)
+                self.delete_frame(frame_to_delete)
             self.selected_frame = Optional.empty()
             if (index == 0):
-                if (len(self.frames) > 0):
-                    self.select_frame(self.frames[0])
+                if (len(self.__frames) > 0):
+                    self.select_frame(self.__frames[0])
             elif (index > 0):
-                self.select_frame(self.frames[index-1])
+                self.select_frame(self.__frames[index-1])
             if (self.selected_frame.is_empty()):
-                frame: Frame = Frame()
-                self.frames.append(frame)
+                frame: Frame = Frame(skeleton=None)
+                self.__frames.append(frame)
                 self.select_frame(frame)
 
     def __handle_select_previous_frame(self):
         print("Select PREVIOUS FRAME")
         if (self.selected_frame.is_present()):
-            index: int = self.frames.index(self.selected_frame.get())
-            if (index > 0):
-                self.select_frame(self.frames[index-1])
+            self.select_previous_frame()
+            
     
     def __handle_select_next_frame(self):
         print("Select NEXT FRAME")
         if (self.selected_frame.is_present()):
-            index: int = self.frames.index(self.selected_frame.get())
-            if (index < (len(self.frames)-1)):
-                self.select_frame(self.frames[index+1])
+            self.select_next_frame()
+            
 
     def handle(self, event: pygame.event.Event):
-        mouse_position = pygame.mouse.get_pos()
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_c):
             self.__handle_duplicate_frame()
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_d):
@@ -397,7 +463,7 @@ class Timeline(pygame.sprite.Sprite, Draw, InputEventHandler):
         self.frame_texture.fill(self.frame_background_color)
         frame_position_x: int = Timeline.FRAME_MARGIN
         
-        for frame in self.frames:
+        for frame in self.__frames:
             if (frame.is_selected):
                 self.frame_texture.fill(self.frame_texture_background_color)
             else:
@@ -433,7 +499,10 @@ class Video(Draw, InputEventHandler):
             if (self.frame_index >= len(self.frames)):
                 self.frame_index = 0
             self.last_render_timestamp = now
-        self.frames[self.frame_index].draw(self.texture)
+        try:
+            self.frames[self.frame_index].draw(self.texture)
+        except IndexError:
+            print(f"Array Size: {len(self.frames)}, requested index: {self.frame_index}")
         self.texture.blit(self.title, (0,0))
         master.blit(self.texture, (self.position.x, self.position.y))
     
@@ -466,12 +535,12 @@ class Workspace(pygame.sprite.Sprite, Draw, InputEventHandler):
         self.mouse_position: Position = Position(0, 0)
 
     def __draw_points(self, master: pygame.Surface):
-        for point in self.frame.points:
+        for point in self.frame.get_points():
             point.draw(master)
 
     def __draw_line_between_each_points(self, master: pygame.Surface):
-        if (len(self.frame.points) > 1):
-            for point in self.frame.points:
+        if (len(self.frame.get_points()) > 1):
+            for point in self.frame.get_points():
                 if (point.parent is not None):
                     parent: Point = point.parent
                     pygame.draw.line(master, pygame.Color(255,255,255), (parent.position.x, parent.position.y), (point.position.x, point.position.y))
@@ -512,18 +581,17 @@ class Workspace(pygame.sprite.Sprite, Draw, InputEventHandler):
         self.__draw_direction_motion_circle(self.texture)
         
         self.texture.blit(self.title, (0, 0))
-        self.position.y
         self.texture.blit(self.mouse_font.render(f"Mouse: (x:{self.mouse_position.x}, y:{self.mouse_position.y})", True, self.mouse_font_color), (0, self.height-self.mouse_font_size))
         master.blit(self.texture, (self.position.x, self.position.y))
 
     def __handle_clear_scene(self):
         print("Delete all POINTS")
-        self.frame.points.clear()
+        self.frame.delete_all_points()
         self.selected_point = Optional.empty()
     
     def __retrieve_children(self, parent: Point) -> list[Point]:
         print(f"Retrieve list of childre for POINT {parent.name}")
-        children: list[Point] = [point for point in self.frame.points if point.parent == parent]
+        children: list[Point] = [point for point in self.frame.get_points() if point.parent == parent]
         for child in children:
             depth_children: list[Point] = self.__retrieve_children(child)
             for depth_child in depth_children:
@@ -535,7 +603,7 @@ class Workspace(pygame.sprite.Sprite, Draw, InputEventHandler):
     def __delete_point(self, point_to_delete: Point, must_delete_tree: bool = True):
         print(f"Delete POINT {point_to_delete.name}")
         try:
-            self.frame.points.remove(point_to_delete)
+            self.frame.delete_point(point_to_delete)
         except:
             pass
         if (must_delete_tree):
@@ -604,27 +672,27 @@ class Workspace(pygame.sprite.Sprite, Draw, InputEventHandler):
     
     def get_selected_point_with_mouse(self) -> Optional[Point]:
         selected_point_with_mouse: Optional[Point] = Optional.empty()
-        selected_points_with_mouse: list[Point] = [point for point in self.frame.points if point.hitbox.collidepoint((self.mouse_position.x, self.mouse_position.y))]
+        selected_points_with_mouse: list[Point] = [point for point in self.frame.get_points() if point.hitbox.collidepoint((self.mouse_position.x, self.mouse_position.y))]
         if (len(selected_points_with_mouse) > 0):
             selected_point_with_mouse = Optional.of_nullable(selected_points_with_mouse[0])
         return selected_point_with_mouse
 
     def __handle_select_point(self, point: Point):
-        for other_point in self.frame.points:
+        for other_point in self.frame.get_points():
             other_point.unselect()
         point.select()
         self.selected_point = Optional.of(point)
 
     def __handle_create_new_point(self):
-        selected_points: list[Point] = [point for point in self.frame.points if point.is_selected()]
-        for point in self.frame.points:
+        selected_points: list[Point] = [point for point in self.frame.get_points() if point.is_selected()]
+        for point in self.frame.get_points():
             point.unselect()
         new_point: Point = Point(Position(self.mouse_position.x, self.mouse_position.y))
         new_point.select()
         self.selected_point = Optional.of(new_point)
         if (len(selected_points) > 0):
             new_point.parent = selected_points[0]
-        self.frame.points.append(new_point)
+        self.frame.add_point(new_point)
 
     def handle(self, event: pygame.event.Event):
         cursor_position: tuple[int] = pygame.mouse.get_pos()
@@ -692,8 +760,9 @@ class App:
         self.is_running: bool = True
         
         print("Creating default frame")
-        frame: Frame = Frame()
-        frame.is_selected = True
+        skeleton: Skeleton = Skeleton()
+        frame: Frame = Frame(skeleton=skeleton)
+        frame.select()
         print("Creating Workspace")
         self.workspace: Workspace = Workspace(App.WORKSPACE_WIDTH, App.WORKSPACE_HEIGHT, Position(0, 0))
         self.workspace.frame = frame
@@ -702,7 +771,7 @@ class App:
         self.video.frames.append(frame)
         print("Creating Timeline")
         self.timeline: Timeline = Timeline(App.TIMELINE_WIDTH, App.TIMELINE_HEIGHT, Position(0, App.WINDOW_HEIGHT-App.TIMELINE_HEIGHT))
-        self.timeline.frames.append(frame)
+        self.timeline.add_frame(frame)
         self.timeline.select_frame(frame)
         self.timeline.add_event_listener_on_selected_frame_change(self.on_selected_frame_change_handler)
     
@@ -720,7 +789,7 @@ class App:
                     self.workspace.handle(event)
                     self.timeline.handle(event)
                     self.video.handle(event)
-                    self.video.set_movie(self.timeline.frames)
+                    self.video.set_movie(self.timeline.get_frames())
 
             self.screen.fill(App.BACKGROUND_COLOR)
 
